cf7e60b837adfac4c719d0c8855faa2c81522bc8
HyperionThS <mystogan.ths@gmail.com>
2017-05-11 09:48:10 +0700

    Glide-Picasso-ImageLoader-Fresco

--- DemoGlide-Picasso/app/src/main/java/com/example/ths/demogilde/MainActivity.java
+++ DemoGlide-Picasso/app/src/main/java/com/example/ths/demogilde/MainActivity.java
@@ -1,6 +1,9 @@
 package com.example.ths.demogilde;
 
 import android.app.ProgressDialog;
+import com.facebook.common.util.UriUtil;
+
+import android.net.Uri;
 import android.os.AsyncTask;
 import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
@@ -10,18 +13,29 @@ import android.widget.Button;
 import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.view.SimpleDraweeView;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
 import com.squareup.picasso.Picasso;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    private Button btGlide, btPicasso;
+    private Button btGlide, btPicasso, btImagesLoader, btFresco;
     private ImageView imageView;
+    private SimpleDraweeView sdvImage;
     public static final String LINK_URL = "https://lh6.ggpht.com/9SZhHdv4URtBzRmXpnWxZcYhkgTQurFuuQ8OR7WZ3R7fyTmha77dYkVvcuqMu3DLvMQ=w300";
-
+    ImageLoader imageLoader;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Fresco.initialize(this);
         setContentView(R.layout.activity_main);
         initView();
+
+        imageLoader =ImageLoader.getInstance();
+        imageLoader.init(ImageLoaderConfiguration.createDefault(getApplicationContext()));
+
+
 
     }
 
@@ -34,6 +48,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
             case R.id.btPicasso:
                 Picasso.with(getApplicationContext()).load(LINK_URL).placeholder(R.drawable.androidify).into(imageView);
                 break;
+            case R.id.btFresco:
+               final Uri uri = Uri.parse(LINK_URL);
+                sdvImage.setImageURI(uri);
+                break;
+            case R.id.btImageLoader:
+                imageLoader.displayImage(LINK_URL,imageView);
+                break;
             default:
                 break;
         }
@@ -42,10 +63,15 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     private void initView() {
         btGlide = (Button) findViewById(R.id.btGilde);
         btPicasso = (Button) findViewById(R.id.btPicasso);
+        btFresco = (Button) findViewById(R.id.btFresco);
+        btImagesLoader = (Button) findViewById(R.id.btImageLoader);
         imageView = (ImageView) findViewById(R.id.imageView);
         btGlide.setOnClickListener(this);
         btPicasso.setOnClickListener(this);
+        btFresco.setOnClickListener(this);
+        btImagesLoader.setOnClickListener(this);
         imageView = (ImageView) findViewById(R.id.imageView);
+        sdvImage = (SimpleDraweeView) findViewById(R.id.sdv_image);
     }
 
 
